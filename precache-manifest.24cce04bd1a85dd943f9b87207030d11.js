685332c5db6913c57a023c8ab4cc202cb7d47433
truongnh-91 <nguyenhuutruong.20111991@gmail.com>
2020-08-18 03:39:26 +0700

    Updates

---
+++ precache-manifest.24cce04bd1a85dd943f9b87207030d11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0302a634d0f135fa66526b8b65436bf1",
+    "url": "/manage-task-react/index.html"
+  },
+  {
+    "revision": "07ce978e6bebc7efc68d",
+    "url": "/manage-task-react/static/css/main.d3ed46f5.chunk.css"
+  },
+  {
+    "revision": "fd4b46fae08e6647cfe9",
+    "url": "/manage-task-react/static/js/2.69770cff.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/manage-task-react/static/js/2.69770cff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07ce978e6bebc7efc68d",
+    "url": "/manage-task-react/static/js/main.92815b61.chunk.js"
+  },
+  {
+    "revision": "c16919a0d9473b630b8e",
+    "url": "/manage-task-react/static/js/runtime-main.aced0525.js"
+  }
+]);
